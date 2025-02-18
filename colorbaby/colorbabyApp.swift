072bb29956be7393ddc87feb65149adad9e7be06
marius <marscobruno@gmail.com>
2025-02-18 14:00:57 +0100

    Initial Commit

---
+++ colorbaby/colorbabyApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorbabyApp.swift
+//  colorbaby
+//
+//  Created by asd on 18/02/25.
+//
+
+import SwiftUI
+
+@main
+struct colorbabyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ colorbabyTests/colorbabyTests.swift
@@ -0,0 +1,17 @@
+//
+//  colorbabyTests.swift
+//  colorbabyTests
+//
+//  Created by asd on 18/02/25.
+//
+
+import Testing
+@testable import colorbaby
+
+struct colorbabyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
